c4b28942d83aa2313e01daeecb8aeed6ed16e6c8
Jeremy Frank <jeremy@subspace.network>
2024-04-03 11:49:31 -0600

    add register_auto_id example

--- auto_identity/__init__.py
+++ auto_identity/__init__.py
@@ -7,6 +7,7 @@ including key generation, Auto ID management, and Auto ID registration and verif
 
 from .key_management import generate_rsa_key_pair, generate_ed25519_key_pair, load_private_key, load_public_key
 from .certificate_management import create_csr, issue_certificate, self_issue_certificate, get_subject_common_name
+from .registry import Registry
 from .utils import der_encode_signature_algorithm_oid
 
 __version__ = '0.1.0'
@@ -21,4 +22,5 @@ __all__ = [
     "self_issue_certificate",
     "get_subject_common_name",
     "der_encode_signature_algorithm_oid",
+    "Registry",
 ]
