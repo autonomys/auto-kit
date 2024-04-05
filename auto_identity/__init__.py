91945fb5d44abd6fac6d508c88d26a4f9c9ebedb
Jeremy Frank <jeremy@subspace.network>
2024-04-05 17:25:54 -0600

    add CertificateManager class for certificate management

--- auto_identity/__init__.py
+++ auto_identity/__init__.py
@@ -16,7 +16,7 @@ from .key_management import (
     pem_to_public_key,
     load_public_key,
     save_key)
-from .certificate_management import create_csr, issue_certificate, self_issue_certificate, get_subject_common_name
+from .certificate_management import CertificateManager
 from .registry import Registry
 from .utils import der_encode_signature_algorithm_oid
 
@@ -32,10 +32,7 @@ __all__ = [
     "pem_to_private_key",
     "pem_to_public_key",
     "key_to_pem",
-    "create_csr",
-    "issue_certificate",
-    "self_issue_certificate",
-    "get_subject_common_name",
+    "CertificateManager",
     "der_encode_signature_algorithm_oid",
     "Registry",
     "Keypair",
--- auto_identity/certificate_management.py
+++ auto_identity/certificate_management.py
@@ -6,134 +6,154 @@ from cryptography.hazmat.primitives.asymmetric import ed25519, rsa
 from .key_management import do_public_keys_match
 
 
-def to_common_name(subject_name):
+class CertificateManager:
     """
-    Converts a subject name to a common name.
-
-    Args:
-        subject_name (str): Subject name for the certificate (common name).
-
-    Returns:
-        str: Common name.
+    Certificate management class.
     """
-    return x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, subject_name)])
 
+    def __init__(self,  certificate=None, private_key=None):
+        """
+        Initializes a certificate issuer.
 
-def prepare_signing_params(private_key):
-    """
-    Prepares the signing parameters based on the key type.
+        Args:
+            certificate(Certificate): Certificate.
+            private_key(PrivateKey): Private key.
+        """
+        self.private_key = private_key
+        self.certificate = certificate
 
-    Args:
-        private_key (PrivateKey): Private key to sign with.
+    @staticmethod
+    def _to_common_name(subject_name):
+        """
+        Converts a subject name to a common name.
 
-    Returns:
+        Args:
+            subject_name(str): Subject name for the certificate(common name).
+
+        Returns:
+            str: Common name.
+        """
+        return x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, subject_name)])
+
+    def _prepare_signing_params(self):
+        """
+        Prepares the signing parameters based on the key type.
+
+        Returns:
             dict: Signing parameters.
-    """
-    if isinstance(private_key, ed25519.Ed25519PrivateKey):
-        return {"private_key": private_key, "algorithm": None}
-    elif isinstance(private_key, rsa.RSAPrivateKey):
-        return {"private_key": private_key, "algorithm": hashes.SHA256()}
-    else:
+        """
+        private_key = self.private_key
+        if isinstance(private_key, ed25519.Ed25519PrivateKey):
+            return {"private_key": private_key, "algorithm": None}
+        if isinstance(private_key, rsa.RSAPrivateKey):
+            return {"private_key": private_key, "algorithm": hashes.SHA256()}
+
         raise ValueError("Unsupported key type for signing.")
 
+    def create_csr(self, subject_name):
+        """
+        Creates a Certificate Signing Request(CSR).
 
-def create_csr(subject_name, private_key):
-    """
-    Creates a Certificate Signing Request (CSR).
+        Args:
+            subject_name(str): Subject name for the CSR(common name).
 
-    Args:
-        subject_name (str): Subject name for the CSR (common name).
-        private_key (PrivateKey): Private key to sign the CSR with.
+        Returns:
+            CertificateSigningRequest: Created X.509 CertificateSigningRequest.
+        """
+        signing_params = self._prepare_signing_params()
+        csr = x509.CertificateSigningRequestBuilder().subject_name(
+            x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, subject_name)])
+        ).sign(**signing_params)
 
-    Returns:
-        CertificateSigningRequest: Created X.509 CertificateSigningRequest.
-    """
-    signing_params = prepare_signing_params(private_key)
-    csr = x509.CertificateSigningRequestBuilder().subject_name(
-        x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, subject_name)])
-    ).sign(**signing_params)
+        return csr
 
-    return csr
+    def issue_certificate(self, csr, validity_period_days=365):
+        """
+        Issues a certificate for Certificate Signing Request(CSR).
 
+        Args:
+            csr(CertificateSigningRequest): Certificate Signing Request.
+            issuer_certificate(Certificate): Issuer certificate.
+            issuer_private_key(PrivateKey): Private key to sign the certificate with .
+            validity_period_days(int, optional): Number of days the certificate is valid. Defaults to 365.
 
-def issue_certificate(csr, issuer_certificate: x509.Certificate, issuer_private_key, validity_period_days=365):
-    """
-    Issues a certificate for Certificate Signing Request (CSR).
+        Returns:
+            Certificate: Created X.509 certificate.
+        """
+        if self.certificate is None:
+            raise ValueError("Certificate is not set.")
+        if self.private_key is None:
+            raise ValueError("Private key is not set.")
 
-    Args:
-        csr (CertificateSigningRequest): Certificate Signing Request.
-        issuer_certificate (Certificate): Issuer certificate.
-        issuer_private_key (PrivateKey): Private key to sign the certificate with.
-        validity_period_days (int, optional): Number of days the certificate is valid. Defaults to 365.
+        issuer_certificate = self.certificate
 
-    Returns:
-        Certificate: Created X.509 certificate.
-    """
+        # Verify that the public key derived from the private key matches the one in the issuer's certificate
+        if not do_public_keys_match(issuer_certificate.public_key(), self.private_key.public_key()):
+            raise ValueError(
+                "Issuer certificate public key does not match the private key used for signing.")
 
-    # Verify that the public key derived from the private key matches the one in the issuer's certificate
-    if not do_public_keys_match(issuer_certificate.public_key(), issuer_private_key.public_key()):
-        raise ValueError(
-            "Issuer certificate public key does not match the private key used for signing.")
+        signing_params = self._prepare_signing_params()
 
-    signing_params = prepare_signing_params(issuer_private_key)
+        certificate = x509.CertificateBuilder().subject_name(
+            csr.subject,
+        ).issuer_name(
+            issuer_certificate.subject,
+        ).public_key(
+            csr.public_key(),
+        ).serial_number(
+            x509.random_serial_number(),
+        ).not_valid_before(
+            datetime.now(timezone.utc),
+        ).not_valid_after(
+            datetime.now(timezone.utc) + timedelta(days=validity_period_days),
+        ).sign(**signing_params)
 
-    certificate = x509.CertificateBuilder().subject_name(
-        csr.subject,
-    ).issuer_name(
-        issuer_certificate.subject,
-    ).public_key(
-        csr.public_key(),
-    ).serial_number(
-        x509.random_serial_number(),
-    ).not_valid_before(
-        datetime.now(timezone.utc),
-    ).not_valid_after(
-        datetime.now(timezone.utc) + timedelta(days=validity_period_days),
-    ).sign(**signing_params)
+        return certificate
 
-    return certificate
+    def self_issue_certificate(self, subject_name: str, validity_period_days=365):
+        """
+        Issues a self-signed certificate for the identity.
 
+        Args:
+            subject_name(str): Subject name for the certificate(common name).
+            private_key(PrivateKey): Private key to sign the certificate with .
+            validity_period_days(int, optional): Number of days the certificate is valid. Defaults to 365.
 
-def self_issue_certificate(subject_name, private_key, validity_period_days=365):
-    """
-    Issues a self-signed certificate for the identity.
+        Returns:
+            Certificate: Created X.509 certificate.
+        """
+        if self.private_key is None:
+            raise ValueError("Private key is not set.")
 
-    Args:
-        subject_name (str): Subject name for the certificate (common name).
-        private_key (PrivateKey): Private key to sign the certificate with.
-        validity_period_days (int, optional): Number of days the certificate is valid. Defaults to 365.
+        signing_params = self._prepare_signing_params()
+        common_name = self._to_common_name(subject_name)
 
-    Returns:
-        Certificate: Created X.509 certificate.
-    """
-    signing_params = prepare_signing_params(private_key)
+        certificate = x509.CertificateBuilder().subject_name(
+            common_name,
+        ).issuer_name(
+            common_name,
+        ).public_key(
+            self.private_key.public_key(),
+        ).serial_number(
+            x509.random_serial_number(),
+        ).not_valid_before(
+            datetime.now(timezone.utc),
+        ).not_valid_after(
+            datetime.now(timezone.utc) + timedelta(days=validity_period_days),
+        ).sign(**signing_params)
 
-    common_name = to_common_name(subject_name)
-    certificate = x509.CertificateBuilder().subject_name(
-        common_name,
-    ).issuer_name(
-        common_name,
-    ).public_key(
-        private_key.public_key(),
-    ).serial_number(
-        x509.random_serial_number(),
-    ).not_valid_before(
-        datetime.now(timezone.utc),
-    ).not_valid_after(
-        datetime.now(timezone.utc) + timedelta(days=validity_period_days),
-    ).sign(**signing_params)
+        self.certificate = certificate
+        return certificate
 
-    return certificate
+    @staticmethod
+    def get_subject_common_name(certificate: x509.Certificate):
+        """
+        Retrieves the common name from the subject of the certificate.
 
+        Args:
+            certificate(x509.Certificate): Certificate to retrieve the common name from .
 
-def get_subject_common_name(certificate: x509.Certificate):
-    """
-    Retrieves the common name from the subject of the certificate.
-
-    Args:
-        certificate (x509.Certificate): Certificate to retrieve the common name from.
-
-    Returns:
-        str: Common name of the certificate.
-    """
-    return certificate.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
+        Returns:
+            str: Common name of the certificate.
+        """
+        return certificate.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
