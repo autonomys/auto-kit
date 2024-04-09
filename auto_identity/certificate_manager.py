b6c5ea9511eb9c9fc19744f6458c40cf503a75e2
Jeremy Frank <jeremy@subspace.network>
2024-04-09 09:02:58 -0600

    remove default_backend usage

--- auto_identity/certificate_manager.py
+++ auto_identity/certificate_manager.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta, timezone
 from cryptography import x509
 from cryptography.x509.oid import NameOID
-from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import ed25519, rsa
 from .key_management import do_public_keys_match
@@ -167,8 +166,7 @@ class CertificateManager:
         Returns:
             An x509.Certificate object.
         """
-        certificate = x509.load_pem_x509_certificate(
-            pem_bytes, default_backend())
+        certificate = x509.load_pem_x509_certificate(pem_bytes)
         return certificate
 
     @staticmethod
