147efe41d727d719e42d96fbc7296de7e907a4f6
Jeremy Frank <jeremy@subspace.network>
2024-05-10 09:47:59 -0600

    improve register auto id example

--- examples/register_auto_id.py
+++ examples/register_auto_id.py
@@ -1,12 +1,19 @@
 import os
+import random
+import string
 from dotenv import load_dotenv
 from auto_identity import generate_ed25519_key_pair, CertificateManager, Registry, Keypair
-
 
 # Configuration
 load_dotenv()
 RPC_URL = os.getenv("RPC_URL")
 KEYPAIR_URI = os.getenv("KEYPAIR_URI")
+
+
+def generate_random_string(length):
+    characters = string.ascii_letters + string.digits
+    random_string = ''.join(random.choices(characters, k=length))
+    return random_string
 
 
 def register(certificate, registry, issuer_id=None):
@@ -16,7 +23,7 @@ def register(certificate, registry, issuer_id=None):
         print(f"Registration successful. {identifer}")
         return identifer
     else:
-        print("Registration failed.")
+        print(f"Registration failed.", receipt)
 
 
 def main():
@@ -26,17 +33,21 @@ def main():
     # Initialize the Registry instance
     registry = Registry(rpc_url=RPC_URL, signer=substrate_keypair)
 
-    keys = generate_ed25519_key_pair()
-    self_issued_cm = CertificateManager(private_key=keys[0])
-    self_issued_cm.self_issue_certificate("test")
+    issuer_keys = generate_ed25519_key_pair()
+    self_issued_cm = CertificateManager(private_key=issuer_keys[0])
+    issuer_name = generate_random_string(10)
+    issuer_cert = self_issued_cm.self_issue_certificate(issuer_name)
     issuer_id = register(self_issued_cm.certificate, registry)
+    print(
+        f"auto id from issuer cert: {CertificateManager.get_certificate_auto_id(issuer_cert)}")
 
     user_keys = generate_ed25519_key_pair()
     user_cm = CertificateManager(private_key=user_keys[0])
-    user_csr = user_cm.create_and_sign_csr("user")
+    user_name = generate_random_string(10)
+    user_csr = user_cm.create_and_sign_csr(user_name)
     user_cert = self_issued_cm.issue_certificate(user_csr)
     CertificateManager.pretty_print_certificate(user_cert)
-    register_user = register(user_cert, registry, issuer_id)
+    _user_id = register(user_cert, registry, issuer_id)
 
     print(
         f"auto id from cert: {CertificateManager.get_certificate_auto_id(user_cert)}")
